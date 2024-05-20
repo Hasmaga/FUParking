d024bc598a0c865f7315f84419e85d0ceb025ffe
tonyandy5630 <tonyandy456@gmail.com>
2024-05-21 04:22:41 +0700

    <modify> json ignore unnessary field in BO

--- FUParkingModel/Object/Common.cs
+++ FUParkingModel/Object/Common.cs
@@ -1,5 +1,6 @@
 ﻿using System.ComponentModel.DataAnnotations.Schema;
 using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
 
 namespace FUParkingModel.Object
 {
@@ -11,9 +12,11 @@ namespace FUParkingModel.Object
         public Guid Id { get; set; }
 
         [Column("CreatedDate")]
+        [JsonIgnore]
         public DateTime CreatedDate { get; set; } = DateTime.Now;
 
         [Column("DeletedDate")]
+        [JsonIgnore]
         public DateTime? DeletedDate { get; set; }
     }
 }
