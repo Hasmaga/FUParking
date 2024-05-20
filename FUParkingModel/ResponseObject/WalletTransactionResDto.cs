208c08a356a1d1700e30d460c5dcc781ae6d8199
tonyandy5630 <tonyandy456@gmail.com>
2024-05-21 04:23:16 +0700

    <feat> add customer view transaction by customer id

---
+++ FUParkingModel/ResponseObject/WalletTransactionResDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Transactions;
+
+namespace FUParkingModel.ResponseObject
+{
+    public class WalletTransactionResDto
+    {
+        public IEnumerable<Transaction>? listTransactions {  get; set; }
+
+    }
+}
--- FUParkingRepository/WalletRepository.cs
+++ FUParkingRepository/WalletRepository.cs
@@ -16,6 +16,32 @@ namespace FUParkingRepository
             _db = db;
         }
 
+        public async Task<Return<List<Transaction>>> GetWalletTransactionByWalletIdAsync(Guid walletId, int pageIndex, int pageSize, DateTime fromDate, DateTime toDate)
+        {
+            Return<List<Transaction>> res = new()
+            {
+                Message = ErrorEnumApplication.SERVER_ERROR
+            };
+            try
+            {
+                // get wallet by id from and to date
+                List<Transaction> transactions = await _db.Transactions.Where(t => t.WalletId.Equals(walletId) && t.CreatedDate >= toDate && t.CreatedDate <= fromDate)
+                                                                .OrderByDescending(t => t.CreatedDate)
+                                                                .Skip((pageIndex - 1) * pageSize)
+                                                                .Take(pageSize)
+                                                                .ToListAsync();
+                res.Data = transactions;
+                res.Message = SuccessfullyEnumServer.GET_OBJECT_SUCCESSFULLY;
+                res.IsSuccess = true;
+                return res;
+            }
+            catch (Exception ex)
+            {
+                res.InternalErrorMessage = ex.Message;
+                return res;
+            }
+        }
+
         public async Task<Return<Wallet?>> GetWalletByCustomerId(Guid customerId)
         {
             Return<Wallet?> res = new()
@@ -27,20 +53,18 @@ namespace FUParkingRepository
                 Wallet? wallet = await _db.Wallets.FirstOrDefaultAsync(w => w.CustomerId.Equals(customerId));
                 if (wallet == null)
                 {
-                    throw new KeyNotFoundException();
+                    res.Message = ErrorEnumApplication.WALLET_NOT_EXIST;
+                    return res;
                 }
                 res.Data = wallet;
                 res.IsSuccess = true;
-
+                return res;
             }
             catch (Exception ex)
             {
-                if (ex is KeyNotFoundException)
-                {
-                    res.Message = ErrorEnumApplication.WALLET_NOT_EXIST;
-                }
+                res.InternalErrorMessage = ex.Message;
+                return res;
             }
-            return res;
         }
 
         public async Task<Return<Wallet>> CreateWalletAsync(Wallet wallet)
