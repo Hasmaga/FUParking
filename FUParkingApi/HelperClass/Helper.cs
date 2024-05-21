2f7a63458a7b3737f20b07544ac4eab083c205bd
KhangBPA <khangbpak2001@gmail.com>
2024-05-21 09:51:11 +0700

    add helper and fix return controller

---
+++ FUParkingApi/HelperClass/Helper.cs
@@ -0,0 +1,24 @@
+﻿using FUParkingModel.Enum;
+using FUParkingModel.ReturnCommon;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace FUParkingApi.HelperClass
+{
+    public class Helper()
+    {
+        public static Return<Dictionary<string, List<string>?>> GetValidationErrors(ModelStateDictionary modelState)
+        {
+            var errors = modelState.ToDictionary(
+                entry => entry.Key,
+                entry => entry.Value?.Errors.Select(error => error.ErrorMessage).ToList()
+            );
+
+            return new Return<Dictionary<string, List<string>?>>
+            {
+                Data = errors,
+                IsSuccess = false,
+                Message = ErrorEnumApplication.INVALID_INPUT
+            };
+        }
+    }
+}
