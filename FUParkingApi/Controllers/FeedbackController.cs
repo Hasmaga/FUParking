538ba134ee3a388bb83696cfbbd62acfd24e5e28
Bùi Thanh Tú <61198577+tonyandy5630@users.noreply.github.com>
2024-05-22 11:57:54 +0700

    Add customer view history feedbacks feature (#37)
    * <modify> change packageid in RequestDTO from type Guid -> string
    
    * <modify> add validation for Guid conversion
    
    * <modify> validation model state changes
    
    * <feat> Add get parking area by id
    
    * <feat> add customer create feedback API
    
    * <feat> Add Pagination constant in Enum
    
    * <fix> Change page index 0 -> 1
    
    * <modify> use pagination constant in controllers
    
    * <feat> add view feedbacks by customer id API
    
    ---------
    
    Co-authored-by: Hasmaga <88260965+Hasmaga@users.noreply.github.com>

--- FUParkingApi/Controllers/FeedbackController.cs
+++ FUParkingApi/Controllers/FeedbackController.cs
@@ -24,6 +24,45 @@ namespace FUParkingApi.Controllers
             _helpperService = helpperService;
         }
 
+        [HttpGet]
+        public async Task<IActionResult> CustomerViewFeedbacksAsync([FromQuery]int pageIndex=Pagination.PAGE_INDEX, [FromQuery]int pageSize=Pagination.PAGE_SIZE)
+        {
+            Return<List<Feedback>> res = new()
+            {
+                Message = ErrorEnumApplication.SERVER_ERROR
+            };
+            try
+            {
+                Guid customerGuid = _helpperService.GetAccIdFromLogged();
+                if (customerGuid == Guid.Empty)
+                {
+                    res.Message = ErrorEnumApplication.NOT_AUTHORITY;
+                    return Unauthorized(res);
+                };
+
+                if (!ModelState.IsValid)
+                {
+                    return UnprocessableEntity(Helper.GetValidationErrors(ModelState));
+                }
+
+                res = await _feedbackService.GetFeedbacksByCustomerIdAsync(customerGuid, pageSize, pageIndex);
+
+                if(res.Message.Equals(ErrorEnumApplication.BANNED))
+                {
+                    return  Forbid();
+                }
+                if(!res.IsSuccess)
+                {
+                    return BadRequest(res);
+                }
+                return Ok(res);
+            }
+            catch(Exception)
+            {
+                return StatusCode(502, res);
+            }
+        }
+
         [HttpPost]
         public async Task<IActionResult> CustomerCreateFeedbackAsync([FromBody] FeedbackReqDto request)
         {
@@ -58,6 +97,5 @@ namespace FUParkingApi.Controllers
                 return StatusCode(502, res);
             }
         }
-
     }
 }
