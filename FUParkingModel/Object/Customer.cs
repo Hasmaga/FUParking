7b6afdb1d206009d978ddb8aa5286905df611b26
tonyandy5630 <tonyandy456@gmail.com>
2024-05-24 06:03:46 +0700

    <modify> change status field to required

--- FUParkingModel/Object/Customer.cs
+++ FUParkingModel/Object/Customer.cs
@@ -26,7 +26,7 @@ namespace FUParkingModel.Object
 
         [Column("StatusCustomer")]
         [JsonIgnore]
-        public string? StatusCustomer { get; set; }
+        public required string StatusCustomer { get; set; }
 
         [JsonIgnore]
         public ICollection<Feedback>? Feedbacks { get; set; }
