a8c1a8560d30233c28bf8ea24d0b8095b2b08858
tonyandy5630 <tonyandy456@gmail.com>
2024-05-24 06:16:02 +0700

    <modify> add required to Message field in Response Object

--- FUParkingModel/ReturnCommon/Return.cs
+++ FUParkingModel/ReturnCommon/Return.cs
@@ -3,7 +3,7 @@
     public class Return<T>
     {
         public T? Data { get; set; }
-        public string? Message { get; set; }
+        public required string Message { get; set; }
         public string? InternalErrorMessage { get; set; }
         public bool IsSuccess { get; set; } = false;
     }
