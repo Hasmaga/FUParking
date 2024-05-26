b7548b12737db263738d1a4881591a284ea27c70
Ducbh <117875129+3mallrice@users.noreply.github.com>
2024-05-26 14:12:42 +0700

    [3mallrice][Create new Gate] (#57)

--- FUParkingService/GateService.cs
+++ FUParkingService/GateService.cs
@@ -1,6 +1,8 @@
 ﻿using FUParkingModel.Enum;
 using FUParkingModel.Object;
+using FUParkingModel.RequestObject;
 using FUParkingModel.ReturnCommon;
+using FUParkingRepository;
 using FUParkingRepository.Interface;
 using FUParkingService.Interface;
 
@@ -68,6 +70,89 @@ namespace FUParkingService
                     IsSuccess = false,
                     Message = ErrorEnumApplication.GET_OBJECT_ERROR,
                     InternalErrorMessage = ex.Message
+                };
+            }
+        }
+
+        public async Task<Return<bool>> CreateGateAsync(CreateGateReqDto req)
+        {
+            try
+            {
+                var isValidToken = _helpperService.IsTokenValid();
+                if (!isValidToken)
+                {
+                    return new Return<bool>
+                    {
+                        IsSuccess = false,
+                        Message = ErrorEnumApplication.NOT_AUTHORITY
+                    };
+                }
+                // Check role 
+                var userlogged = await _userRepository.GetUserByIdAsync(_helpperService.GetAccIdFromLogged());
+                if (userlogged.Data == null || userlogged.IsSuccess == false)
+                {
+                    return new Return<bool>
+                    {
+                        IsSuccess = false,
+                        Message = ErrorEnumApplication.NOT_AUTHORITY
+                    };
+                }
+
+                if (!Auth.AuthSupervisor.Contains(userlogged.Data.Role?.Name ?? ""))
+                {
+                    return new Return<bool> { IsSuccess = false, Message = ErrorEnumApplication.NOT_AUTHORITY };
+                }
+
+                //create gate
+                var gateList = await _gateRepository.GetAllGateAsync();
+                if (gateList.Data != null && gateList.IsSuccess)
+                {
+                    bool isGateNameExist = gateList.Data.Any(x => x.Name.Equals(req.Name, StringComparison.OrdinalIgnoreCase));
+
+                    if (isGateNameExist == true)
+                    {
+                        return new Return<bool>
+                        {
+                            IsSuccess = false,
+                            Message = ErrorEnumApplication.GET_OBJECT_ERROR
+                        };
+                    }
+                }
+
+                var gate = new Gate
+                {
+                    Name = req.Name,
+                    Description = req.Description,
+                    ParkingAreaId = req.ParkingAreaId,
+                    GateTypeId = req.GateTypeId,
+                };
+
+                var result = await _gateRepository.CreateGateAsync(gate);
+
+                if (result.IsSuccess)
+                {
+                    return new Return<bool>
+                    {
+                        IsSuccess = true,
+                        Data = true,
+                        Message = SuccessfullyEnumServer.CREATE_OBJECT_SUCCESSFULLY
+                    };
+                }
+                else
+                {
+                    return new Return<bool>
+                    {
+                        IsSuccess = false,
+                        Message = ErrorEnumApplication.SERVER_ERROR
+                    };
+                }
+            }
+            catch
+            {
+                return new Return<bool>
+                {
+                    IsSuccess = false,
+                    Message = ErrorEnumApplication.SERVER_ERROR
                 };
             }
         }
--- FUParkingService/Interface/IGateService.cs
+++ FUParkingService/Interface/IGateService.cs
@@ -1,12 +1,12 @@
 ﻿using FUParkingModel.Object;
+using FUParkingModel.RequestObject;
 using FUParkingModel.ReturnCommon;
-using Microsoft.Identity.Client;
-using System.Threading.Tasks.Sources;
 
 namespace FUParkingService.Interface
 {
     public interface IGateService
     {
         Task<Return<IEnumerable<Gate>>> GetAllGate();
+        Task<Return<bool>> CreateGateAsync(CreateGateReqDto req);
     }
 }
