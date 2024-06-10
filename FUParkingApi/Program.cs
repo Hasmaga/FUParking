2b5ccd943830cb859449909ba43283aea997944a
tonyandy5630 <tonyandy456@gmail.com>
2024-06-10 16:33:24 +0700

    <modify> move to access cf to dev

--- FUParkingApi/Program.cs
+++ FUParkingApi/Program.cs
@@ -138,9 +138,8 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+    AccessCloudflare.Access();
 }
-
-AccessCloudflare.Access();
 
 app.UseHttpsRedirection();
 
