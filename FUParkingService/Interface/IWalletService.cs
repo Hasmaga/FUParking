6c7ef40e40ecbe6fdb68da803bd5ae5bc99d9dcf
KhangBPA <khangbpak2001@gmail.com>
2024-07-10 01:15:57 +0700

    FIx

--- FUParkingService/Interface/IWalletService.cs
+++ FUParkingService/Interface/IWalletService.cs
@@ -6,8 +6,7 @@ using FUParkingModel.ReturnCommon;
 namespace FUParkingService.Interface
 {
     public interface IWalletService
-    {
-        Task<Return<List<Transaction>>> GetWalletTransactionByCustomerIdAsync(string? customerId, int pageIndex, int pageSize, int numberOfDate);
+    {        
         Task<Return<GetWalletTransResDto>> GetTransactionWalletMainAsync(GetListObjectWithFillerDateReqDto req);
         Task<Return<GetWalletTransResDto>> GetTransactionWalletExtraAsync(GetListObjectWithFillerDateReqDto req);
     }
