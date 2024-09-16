03f6e0380626fe77ad7f3ddcf84f7078624f0fb1
Maotou <93722462+maotou-spy@users.noreply.github.com>
2024-09-16 19:00:42 +0700

    [amotou][update card checker] (#114)

--- FUParkingModel/ResponseObject/Card/GetCardByCardNumberResDto.cs
+++ FUParkingModel/ResponseObject/Card/GetCardByCardNumberResDto.cs
@@ -2,6 +2,7 @@
 {
     public class GetCardByCardNumberResDto
     {
+        public required Guid CardId { get; set; }
         public required string CardNumber { get; set; }       
         public required string Status { get; set; }
         public Guid? SessionId { get; set; }
@@ -9,8 +10,8 @@
         public string? SessionVehicleType { get; set; }
         public DateTime SessionTimeIn { get; set; }
         public string? SessionGateIn { get; set; }
-        public string? SessionCustomerName { get; set; }
-        public string? SessionCustomerEmail { get; set; }
+        public DateTime? SessionTimeOut { get; set; }
+        public string? SessionGateOut { get; set; }
         public string? ImageInUrl { get; set; }
         public string? ImageInBodyUrl { get; set; }
         public string? SessionStatus { get; set; } 
